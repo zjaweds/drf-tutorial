d7d146ee809d57f8a51cd26abda664c86cd99782
Jawed Alam <zjaweds@outlook.com>
2025-05-03 20:18:20 +0530

    Bug fix

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class SnippetSerializer(serializers.ModeSerializer):
+class SnippetSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
     code = serializers.CharField(style={'base_template': 'textarea.html'})
